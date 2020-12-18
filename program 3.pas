6c55966ee030ce2641da1fc5ec19666a5423bbd9
Lorviral <75840599+Lorviral@users.noreply.github.com>
2020-12-18 14:56:28 +0300

    Максимальный элемент одномерного массива Pascal

---
+++ program 3.pas
@@ -0,0 +1,17 @@
+﻿program ne_kratnie_7;
+  const sz=100;
+  var
+  arr:array [1..sz] of integer;
+  i,n,max:integer;
+  
+  begin
+    write ('Введите кол-во элементов массива:'); readln (n);
+    write ('Введите элементы массива:');
+    for i:=1 to n do
+    read(arr[i]);
+    
+    max:=arr[1];
+    for i:= 2 to n do
+      if arr[i]>max then max:=arr[i];
+    writeln('Максимальный элемент одномерного массива: ',max)
+  end.
